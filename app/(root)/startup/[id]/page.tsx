db54305cf3bfdb79d4659c0cac6eff79ce93c9d6
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2024-11-02 13:26:29 +0530

    update next config

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -7,7 +7,7 @@ import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
-import View from "@/components/view";
+import View from "@/components/View";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
